b4ada59731f3e0e14cf944c30787ad259c940f0d
Dong Le <61934131+dongle47@users.noreply.github.com>
2023-03-02 19:07:48 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+import { Panorama } from "./Panorama";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Panorama />
+    </div>
+  );
+}
